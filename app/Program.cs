e75ff41ff0ea3f4e0a565f3b1ea6d27746020571
alarmsdk <137072432+alarmsdk@users.noreply.github.com>
2024-01-30 17:46:05 +0000

    project initialization

---
+++ app/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+Console.WriteLine("Hello, World!");
+
+var client = new MongoClient("mongodb://localhost:27017?replicaSet=rs0");
+
+var database = client.GetDatabase("test");
+
+var collection = database.GetCollection<object>("event");
+
+using (var cursor = collection.Watch())
+{
+    foreach (var change in cursor.ToEnumerable())
+    {
+        Console.WriteLine(change.ToString());
+        // process change event
+    }
+}
+
+
+Console.WriteLine("İşlem tamam");
